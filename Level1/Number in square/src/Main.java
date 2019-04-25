042fd1906e954c29f196bbf47e61d0b64e18c5f8
Ivan Izdepskyi <iizdepskyy112@gmail.com>
2019-04-25 14:18:34 +0300

    level 1

---
+++ Level1/Number in square/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println(sqr(a));
+    }
+    public static int sqr(int a){
+        return a*a;
+    }
+}
