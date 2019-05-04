4c4cd7f71a54d1c5a3dc2e817fcc876657554589
Ivan Izdepskyi <iizdepskyy112@gmail.com>
2019-05-04 16:24:55 +0300

    Level 2

---
+++ Level 2/Implement method print/src/Main.java
@@ -0,0 +1,23 @@
+
+/*Реализуй метод print. Метод должен вывести на экран переданную строку 4 раза. Каждый раз с новой строки.
+        Требования:
+        1. Программа должна выводить текст на экран.
+        2. Метод main не должен вызывать System.out.println или System.out.print.
+        3. Метод print должен выводить текст на экран.
+        4. Метод main должен вызвать метод print класса Solution только один раз.
+        5. Метод print должен выводить на экран строку 4 раза. Каждый раз с новой строки.
+
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        print("MBT Revolution");
+    }
+
+    public static void print(String s){
+        for (int i = 0; i<4; i++){
+            System.out.println(s);
+        }
+    }
+}
