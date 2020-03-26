414ead97c329150e5e312eeaf7b2f70554561f26
SwAppCoder <iizdepskyy112@gmail.com>
2020-03-26 20:11:50 +0200

    0501

---
+++ Level 5/0501/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+    String name;
+    int age;
+    int weight;
+    int strength;
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
